0e1c29a6e04c95ce3eb9e6042dcdb077f0442216
lakshit_nagar <lakshitnagar@gmail.com>
2018-11-29 14:50:41 +0530

    mock up look and feel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TreeComponent } from './tree/tree.component';
+import { MaterialTreeComponent } from './material-tree/material-tree.component';
+import { CdkTreeComponent } from './cdk-tree/cdk-tree.component';
 
 const routes: Routes = [
-  { path: 'tree', component: TreeComponent }
+  { path: 'tree', component: TreeComponent },
+  { path: 'material-tree', component: MaterialTreeComponent },
+  { path: 'cdk-tree', component: CdkTreeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AngularCdkModule } from './material/angular-cdk/angular-cdk.module';
 import { AppComponent } from './app.component';
 import { TreeComponent } from './tree/tree.component';
 import { CreateTreeComponent } from './create-tree/create-tree.component';
+import { MaterialTreeComponent } from './material-tree/material-tree.component';
+import { CdkTreeComponent } from './cdk-tree/cdk-tree.component';
+import { MainDashComponent } from './main-dash/main-dash.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TreeComponent,
-    CreateTreeComponent
+    CreateTreeComponent,
+    MaterialTreeComponent,
+    CdkTreeComponent,
+    MainDashComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularCdkModule // ALWAYS INCLUDE THIS AFTER "BrowserModule"
   ],
   providers: [],
   bootstrap: [AppComponent]
